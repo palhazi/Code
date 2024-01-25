9feb207aa09f2d59b0c9b924b693b08e103ae9ba
Pálházi Tamás <palhazi@yahoo.com>
2024-01-25 20:59:14 +0100

    djskskhdfksh

--- CreateOrder.py
+++ CreateOrder.py
@@ -1,6 +1,7 @@
 from ibapi.client import EClient
 from ibapi.wrapper import EWrapper
 from ibapi.contract import Contract
+from ibapi.order import Order
 import threading
 
 class TradingApp(EClient, EWrapper):
@@ -25,7 +26,7 @@ while (app.nextValidIdOrderId == None):
 
 print("Connection established!")
 
-# Define an Apple stock contract
+# Define an Apple stock contract ---- https://interactivebrokers.github.io/tws-api/
 
 c1 = Contract()
 
@@ -34,3 +35,12 @@ c1.secType="STK"
 c1.currency="USD"
 c1.exchange="SMART"
 c1.primaryExchange="NASDAQ"
+
+# Create limit order
+
+o1 = Order()
+
+o1.action = "BUY"
+o1.orderType = "LMT"
+o1.totalQuantity = 10
+o1.lmtPrice = 200
